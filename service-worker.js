b4f44e53de19178c2d0c3242a85c787c6a4a7d46
amalkishan123-maker <amalkishan123@gmail.com>
2026-01-14 18:55:40 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,5 @@
-const CACHE = "foodcheck-v1";
-const FILES = ["./","index.html","manifest.json"];
-
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>res || fetch(e.request))
-  );
-});
+self.addEventListener("fetch", e => {});
